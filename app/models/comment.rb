dce53cddb87e0d187ca4b67782caa2b385d18630
nevillealee <nevillealee@gmail.com>
2017-06-23 02:27:42 +0000

    humanized var name for drop down ratings menu so that ux is changable without the need to rename database values

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'one star': '1_star',
+        'two_stars': '2_stars',
+        'three_stars': '3_stars',
+        'four_stars': '4_stars',
+        'five_stars': '5_stars'
+    }
+    
+      def humanized_rating
+        RATINGS.invert[self.rating]
+      end
+    
 end
