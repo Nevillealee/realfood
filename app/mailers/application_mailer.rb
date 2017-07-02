6d6c8f64306dc4f3d77aa3b50696b9f68e88d0b0
nevillealee <nevillealee@gmail.com>
2017-07-02 21:52:30 +0000

    notificationmailer adjustments made

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@realfoodapp.com"
   layout 'mailer'
   
-  def comment_added
-    mail(to: "nevillealee@gmail.com", subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, 
+      subject: "A comment has been added to #{@place.name}")
   end
   
 end
